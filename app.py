4a6219ffc6db2e5d4b9b15e51bc6b0ac0a4f5252
A <sneha.c.a@capgemini.com>
2022-01-31 22:43:36 +0530

    intial commit setup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import base64
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
@@ -100,4 +101,108 @@ def PredictB64():
 
 
 if __name__ == '__main__':
+=======
+import base64
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import  cv2
+import keras
+import skimage
+
+
+import io
+import traceback
+app = Flask(__name__)
+
+global classes
+classes = ["printer", "computer monitor", "computer keyboard"]
+import warnings
+warnings.filterwarnings('ignore')
+
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.visualize import display_instances
+
+
+"""
+Mask R-CNN
+Display and Visualization Functions.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+"""
+
+import os
+import sys
+import random
+import itertools
+import colorsys
+
+import numpy as np
+from skimage.measure import find_contours
+import matplotlib.pyplot as plt
+from matplotlib import patches,  lines
+from matplotlib.patches import Polygon
+import IPython.display
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+
+
+from PIL import Image
+import numpy as np
+
+
+
+@app.route('/')
+def upload_form():
+    return render_template("singlefile_index.html")
+
+@app.route('/display_result_imgs', methods=['POST'])
+def PredictB64():
+    try:
+        #save it to tmp dir
+        from test import detect
+        files = request.files.getlist("files")
+        base64_l = []
+        print(files)
+        for f in files:
+            print("f",f)
+            file = f.read()
+
+            npimg = np.fromstring(file, np.uint8)
+            # convert numpy array to image
+            decoded = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+            print("before decode",decoded)
+            frame = detect(decoded)
+            print(frame)
+            print(type(frame))
+
+            rawBytes = io.BytesIO()
+            print("the image",frame)
+            frame.save(rawBytes, "JPEG")
+            rawBytes.seek(0)
+            img_base64 = base64.b64encode(rawBytes.read())
+            print(type(img_base64))
+            img_str = img_base64.decode('utf-8')
+
+            base64_l.append(img_str)
+
+        return render_template("display_result.html",data=base64_l)
+    except Exception as e:
+        print("+++++++++++++++++++++++++++")
+        print(traceback.format_exc())
+        return "err"
+
+
+
+if __name__ == '__main__':
+>>>>>>> 39c9c927bd4b42f0ffe9f28acd15dcaa8c3888b8
     app.run()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from PIL import Image
 import numpy as np
 import tensorflow as tf
@@ -81,4 +82,89 @@ def detect(decoded):
     except Exception as e:
         print("======================")
         print(traceback.format_exc())
+=======
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import  cv2
+import keras
+import skimage
+import io
+import traceback
+
+classes = ["printer", "computer monitor", "computer keyboard"]
+import warnings
+warnings.filterwarnings('ignore')
+
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.visualize import display_instances
+from mrcnn import visualize
+
+class PredictionConfig(Config):
+    # define the name of the configuration
+    NAME = "cfg_new"
+    # number of classes (background + Inlet)
+    NUM_CLASSES = len(classes) + 1
+    # simplify GPU config
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+config = PredictionConfig()
+config.display()
+
+#intializing config info
+cfg = PredictionConfig()
+model = MaskRCNN(mode='inference', model_dir='./', config=cfg)
+# # load model weights
+model.load_weights("./printer_monitor_keyboard/weights/weights.h5", by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+
+
+# def display_instances(image, boxes, masks, ids, names, scores):
+#     n_instances = boxes.shape[0]
+#     colors = visualize.random_colors(n_instances)
+#     if not n_instances:
+#         print('NO INSTANCES TO DISPLAY')
+#     else:
+#         assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+#     for i, color in enumerate(colors):
+#         if not np.any(boxes[i]):
+#             continue
+#         y1, x1, y2, x2 = boxes[i]
+#         print('++++++++++++++++++++++++++++++++++++++++++++++++')
+#         print(i)
+#         print(ids)
+#         print(scores)
+#         label = names[ids[i]]
+#         score = scores[i] if scores is not None else None
+#         caption = '{} {:.2f}'.format(label, score) if score else label
+#         mask = masks[:, :, i]
+#
+#         image = visualize.apply_mask(image, mask, color)
+#         image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+#         image = cv2.putText(
+#             image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+#         )
+#
+#     return image
+
+def detect(decoded):
+    try:
+        class_names = ["no data","printer", "computer monitor", "computer keyboard"]
+        print("decoaded",decoded)
+        r = model.detect([decoded], verbose=1)[0]
+        print(r['class_ids'])
+        masked_frame = display_instances(decoded, r['rois'], r['masks'], r['class_ids'],
+                                        class_names, r['scores'])
+        try:
+            cv2.imwrite("filename.jpg", masked_frame)
+        except Exception as e:
+            print("img write err"+str(e))
+        # # You may need to convert the color.
+        # masked_frame = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2RGB)
+        # im_pil = Image.fromarray(masked_frame)
+        return masked_frame
+    except Exception as e:
+        print("======================")
+        print(traceback.format_exc())
+>>>>>>> 39c9c927bd4b42f0ffe9f28acd15dcaa8c3888b8
         return str(traceback.format_exc())
